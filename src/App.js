0e78a1a08e9d9f5d7c893bbf44314ff8d6c3f50a
lion5232 <aa52329039@gmail.com>
2024-10-14 16:08:04 +0900

    마운트에 대해서

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state= {
+    count:0
+  };
+
+  add= () => {
+    this.setState(current =>({count: current.count +1}));
+  };
+  minus= () => {
+    this.setState(current=>({count:current.count -1}));
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>add</button>
+        {/* this.add()는 즉시 발동, this.add는 클릭했을때만 발동 */}
+        <button onClick={this.minus}>minus</button>
+      </div>
+      );
+  }
 }
 
 export default App;
+
+// 클래스 형은 Class 선언, render()함수 선언, Component 상속을 해야하고,
+// 상태관리를 위해 constructor, 생성자 메소드 전언 등…이 필요합니다
+
+// 컴포넌트 실행 순서 : constructor <- render <- comopnentDiMount
+
+//update : state가 변경되다.
---
+++ src/Test.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Test() {
+    return(
+        <h3>
+            Test file
+        </h3>
+    );
+}
+
+export default Test;
